e52a6cd2bb11d68b4589b5680bb71f2254704a89
Sharruken <Sharruken@gmail.com>
2018-07-30 10:56:03 -0400

    Add files via upload

--- Converter/Converter.py
+++ Converter/Converter.py
@@ -4,28 +4,24 @@ import shutil
 import random
 import subprocess
 
-files = os.listdir(os.getcwd())
+Music = os.listdir("Music")
 under_score = ['(',')','[',']'] 
 dot = [' ']
 root = os.curdir
-mp3Folder = os.path.join(root, "MP3s")
-wavFolder = os.path.join(root, "WAVs")
-oggFolder = os.path.join(root, "OGGs")
-flacFolder = os.path.join(root, "FLACs")
 
-#subprocess.call("FFMPEGConvert.bat")
+subprocess.call("FFMPEGConvert.bat")
 
-
-for f in files:
+os.chdir("Music")
+for f in Music:
     copy_f = f
     for char in copy_f:
         if (char in dot): copy_f = copy_f.replace(char, '.')
         if (char in under_score): copy_f = copy_f.replace(char,'_')
     os.rename(f,copy_f)
+os.chdir("..")
 
 
-
-for root, dir, files in os.walk("WAVS"):
+for root, dir, files in os.walk(os.curdir):
    for name in files:
       subprocess.call(["wav2msu.exe" , os.path.join(root, name)])
       print name
