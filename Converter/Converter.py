4e19cb5bd785a681276cf48817d0c81b2830a8f2
Sharruken <Sharruken@gmail.com>
2018-08-04 15:20:28 -0400

    Add files via upload

--- Converter/Converter.py
+++ Converter/Converter.py
@@ -4,12 +4,15 @@ import shutil
 import random
 import subprocess
 
+root = os.curdir
 Music = os.listdir("Music")
+MP3s = os.path.join(root, "MP3s")
 under_score = ['(',')','[',']'] 
 dot = [' ']
-root = os.curdir
-
-subprocess.call('FFMPEG.exe FOR /F "tokens=*" %G IN ("dir Music /b *.mp*") DO ffmpeg -i "MP3s\%G" -c:a libvorbis -q:a 4 "Music\%~nG.ogg"')
+wav2msuExtensions = (".wav",".ogg",".flac")
+ffmpegExtensions = (".mp3",".mp4")
+normalizeExtensions = (".mp3",".mp4",".wav")
+MP3Folder = os.path.abspath("Music")
 
 os.chdir("Music")
 for f in Music:
@@ -20,8 +23,17 @@ for f in Music:
     os.rename(f,copy_f)
 os.chdir("..")
 
+for root, dir, files in os.walk(os.curdir):
+    for name in files:
+        if name.endswith(normalizeExtensions):
+            subprocess.call(["normalize.exe" , "-a 16dBFS", os.path.join(root, name)])
 
 for root, dir, files in os.walk(os.curdir):
    for name in files:
-      subprocess.call(["wav2msu.exe" , os.path.join(root, name)])
-      print name
+       if name.endswith(ffmpegExtensions):
+           subprocess.call(["mpg123.exe", "-w", os.path.join(MP3Folder, name).strip(".mp3")+".wav", os.path.join(MP3Folder, name)])
+
+for root, dir, files in os.walk(os.curdir):
+   for name in files:
+       if name.endswith(wav2msuExtensions):
+           subprocess.call(["wav2msu.exe" , os.path.join(root, name)])
